9f3b463877ac76a3b45991233cba1656d98cb5cf
iki020904 <muhammadrifqiaziz15@gmail.com>
2023-12-06 21:51:26 +0700

    file baru

---
+++ Lab9_php_modular/home.php
@@ -0,0 +1,10 @@
+
+<div class="content">
+<h2>hello muhammad rifqi<section id="hero">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lorem
+elit, iaculis innisl volutpat, malesuada tincidunt arcu. Proin in leo fringilla,
+vestibulum mi porta, faucibus felis. Integer pharetra est nunc, nec pretium nunc
+pretium ac.</p>
+<a href="home.html" class="btn btn-large">Learn more &raquo;</a>
+</section>
+  
---
+++ Lab9_php_praktikum/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>Created by <a href="">Muhammad Rifqi aziz</a> | &copy; 2023</p>
+        </footer>
+    </div>
+</body>
+</html>
